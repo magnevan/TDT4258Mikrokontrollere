940b877317c6d7b425cbf13f09982c4b33f502d7
Magne Vikjord <mvikjord@gmail.com>
2014-04-08 22:02:48 +0200

    Gamepad code, unfinished

--- OSELAS.BSP-EnergyMicro-Gecko/local_src/driver-gamepad-1.0/driver-gamepad.c
+++ OSELAS.BSP-EnergyMicro-Gecko/local_src/driver-gamepad-1.0/driver-gamepad.c
@@ -1,41 +1,70 @@
-/*
- * This is a demo Linux kernel module.
- */
-
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/fs.h>
+#include <linux/types.h>
+#include <linux/cdev.h>
 
-/*
- * template_init - function to insert this module into kernel space
- *
- * This is the first of two exported functions to handle inserting this
- * code into a running kernel
- *
- * Returns 0 if successfull, otherwise -1
- */
+ /* For now, we can just have this as a standalone variable */
+ static struct cdev gamepad_cdev;
 
-static int __init template_init(void)
+ static struct file_operations gamepad_fops =
+ {
+    .owner = THIS_MODULE,
+    .open = gamepad_open,
+    .read = gamepad_read,
+    .write = gamepad_write,
+    .release = gamepad_release,
+ }
+ 
+ /* The allocated device number, mayor and minor */
+ dev_t dev_num;
+ 
+static int __init gamepad_init(void)
 {
-	printk("Hello World, here is your module speaking\n");
-	return 0;
+   /* TODO magne err handling */
+
+   /* Allocate device region w/number */
+   alloc_chrdev_region(&dev_num, 0, 1, "gamepad");
+    
+   /* Register device in the system (NOTE: do last)*/
+   cdev_init(&gamepad_cdev, &gamepad_fops);
+   gamepad_cdev.owner = THIS_MODULE;
+   gamepad_cdev.ops = &gamepad_fops;
+   cdev_add(&gamepad_cdev, dev_num, 1);
+    
+   printk(KERN_INFO "Gamepad kernel module initialized\n");
+   return 0;
 }
 
-/*
- * template_cleanup - function to cleanup this module from kernel space
- *
- * This is the second of two exported functions to handle cleanup this
- * code from a running kernel
- */
-
-static void __exit template_cleanup(void)
+static void __exit gamepad_cleanup(void)
 {
-	 printk("Short life for a small module...\n");
+    cdev_del(&gamepad_cdev);
+    unregister_chrdev_region(dev_num, 1);
+    printk("Gamepad kernel module cleaned up\n");
 }
 
-module_init(template_init);
-module_exit(template_cleanup);
+/* FOPS */
+static int gamepad_open(struct inode *inode, struct file *filp) {return 0;}
+static int gamepad_release(struct inode *inode, struct file *filp) {return 0;}
 
-MODULE_DESCRIPTION("Small module, demo only, not very useful.");
+static int gamepad_read(
+    struct file *filp, char __user *buff, 
+    size_t count, loff_t *offp
+) {
+    return 1;
+}
+
+static int gamepad_write(
+    struct file *filp, char __user *buff, 
+    size_t count, loff_t *offp
+) {
+    return 1;
+}
+
+module_init(gamepad_init);
+module_exit(gamepad_cleanup);
+
+MODULE_DESCRIPTION("Kernel module for the gamepad used in TDT4258 fall 2014.");
 MODULE_LICENSE("GPL");
 
