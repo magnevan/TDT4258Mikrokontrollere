96a30c6373f53f2b9d410927b16fbab1d065b4a6
Magne Vikjord <mvikjord@gmail.com>
2014-04-10 14:38:27 +0200

    small update. just pushing before I go to the lab

--- OSELAS.BSP-EnergyMicro-Gecko/local_src/driver-gamepad-1.0/driver-gamepad.c
+++ OSELAS.BSP-EnergyMicro-Gecko/local_src/driver-gamepad-1.0/driver-gamepad.c
@@ -4,9 +4,14 @@
 #include <linux/fs.h>
 #include <linux/types.h>
 #include <linux/cdev.h>
+#include <linux/ioport.h>
+
+#include "efm32gg.h"
+
+#define GPIO_LENGTH = 0x120
 
  /* For now, we can just have this as a standalone variable */
- static struct cdev gamepad_cdev;
+struct cdev gamepad_cdev;
 
  static struct file_operations gamepad_fops =
  {
@@ -18,7 +23,11 @@
  }
  
  /* The allocated device number, mayor and minor */
- dev_t dev_num;
+dev_t dev_num;
+ 
+struct class *cl;
+
+void *gpio_pc;
  
 static int __init gamepad_init(void)
 {
@@ -26,12 +35,20 @@ static int __init gamepad_init(void)
 
    /* Allocate device region w/number */
    alloc_chrdev_region(&dev_num, 0, 1, "gamepad");
+   
+   /* Request memory region */
+   request_mem_region(GPIO_PC_BASE, GPIO_LENGTH, "gamepad");
+   gpio_pc = ioremap_nocache(GPIO_PC_BASE, GPIO_LENGTH);
     
    /* Register device in the system (NOTE: do last)*/
    cdev_init(&gamepad_cdev, &gamepad_fops);
    gamepad_cdev.owner = THIS_MODULE;
    gamepad_cdev.ops = &gamepad_fops;
    cdev_add(&gamepad_cdev, dev_num, 1);
+   
+   /* Making the driver visible to user space */
+   cl = class_create(THIS_MODULE, "gamepad");
+   device_create(cl, NULL, dev_num, NULL, "gamepad");
     
    printk(KERN_INFO "Gamepad kernel module initialized\n");
    return 0;
@@ -39,8 +56,12 @@ static int __init gamepad_init(void)
 
 static void __exit gamepad_cleanup(void)
 {
+    iounmap(gpio_pc);
+    release_mem_region(GPIO_PC_BASE, GPIO_LENGTH);
     cdev_del(&gamepad_cdev);
     unregister_chrdev_region(dev_num, 1);
+    
+    
     printk("Gamepad kernel module cleaned up\n");
 }
 
@@ -52,6 +73,9 @@ static int gamepad_read(
     struct file *filp, char __user *buff, 
     size_t count, loff_t *offp
 ) {
+    /* GPIO_PC_DIN */
+    unsigned int button_state = ioread8(gpio_pc + 0x1c);
+    copy_to_user(buff, &button_state, 1);
     return 1;
 }
 
