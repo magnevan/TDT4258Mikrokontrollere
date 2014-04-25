a25282f3c86935bec3a5cafdcc6caef450ce3d31
Magne Vikjord <mvikjord@gmail.com>
2014-04-25 12:22:22 +0200

    finished picture generating script

--- img/script.py
+++ img/script.py
@@ -1,12 +1,25 @@
 import Image
-i = Image.open("bb.png")
-w,h = i.size
+from glob import glob
 
-for y in xrange(h):
-    val = 0
-    for x in xrange(w):
-        print i.getpixel((x,y))
-        if i.getpixel((x,y))[3] > 128:
-            val &= 1 << (w-x-1)
+def printline(val, w):
+    line = [' ']*w
+    for i in xrange(w):
+        if val & (1<<i) > 0:
+            line[i] = 'o'
+    print ''.join(line)
 
-    print val
+with open('pictures.txt', 'w') as outf:
+    for filename in glob('*.png'):
+        i = Image.open(filename)
+        w,h = i.size
+
+        outf.write('%s\n' % filename)
+        for y in xrange(h):
+            val = 0
+            for x in xrange(w):
+                #print i.getpixel((x,y))
+                if i.getpixel((x,y))[3] > 128:
+                    val |= 1 << (w-x-1)
+
+            outf.write('%d,\n' % val)
+        outf.write('\n\n')
